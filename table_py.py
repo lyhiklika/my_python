06843dd348685a01c7c10ee078b8be3ddddc6e84
Anjelika Kuvaldina <anhelika.kuvaldina@mail.ru>
2019-09-18 22:33:05 +0300

    init commit

---
+++ table_py.py
@@ -0,0 +1,14 @@
+value= input ('введите pH: ')
+if value :
+                pH =int (value)
+                if pH==3:
+                    print ('Li')
+                elif pH==25:
+                    print ('Mn')
+                elif pH==80:
+                    print ('Hg')
+                elif pH==17:
+                    print ('Cl')
+                else: print('что это?')
+else: print ('введите pH!')
+              
