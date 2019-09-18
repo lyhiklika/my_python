0dd82971f11f7fbfac7d4fd5ad0d36c4089fcc20
Anjelika Kuvaldina <anhelika.kuvaldina@mail.ru>
2019-09-19 00:29:48 +0300

    init commit

---
+++ call_tel.py
@@ -0,0 +1,16 @@
+x=int (input ("введите код города"))
+y=float(input("введите длительность разговора"))
+if x:
+    if y:
+        if x==343:
+            print (15*y)
+        elif x==381:
+            print (18*y)
+        elif x==473:
+            print (13*y)
+        elif x==485:
+            print (11*y)
+    else:
+        print ("введите код города!!")
+else:
+    print ("введите длительность разговора!!")
