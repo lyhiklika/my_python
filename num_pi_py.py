d04ee9510f8af204bf21bf65841eabc4346bb42f
Anjelika Kuvaldina <anhelika.kuvaldina@mail.ru>
2019-09-19 10:13:18 +0300

    init commit

---
+++ num_pi_py.py
@@ -0,0 +1,8 @@
+def pi(x):
+    from math import pi
+    return f'{pi:.{x}f}'
+y=input ("Введите количество знаков после запятой: ")
+if y:
+    print (pi(y))
+else:
+    print ("Введите количество знаков после запятой!!:")
